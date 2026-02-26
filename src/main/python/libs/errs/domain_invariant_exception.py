09cbe52d19ddb55c49d92776a925c13d8521326d
Максим Заикин <zaikinmaxv@gmail.com>
2026-02-26 21:24:53 +0500

    Update and fix libs

--- src/main/python/libs/errs/domain_invariant_exception.py
+++ src/main/python/libs/errs/domain_invariant_exception.py
@@ -1,3 +1,6 @@
+from libs.errs.error import Error
+
+
 class DomainInvariantError[**P](Exception):
-    def __init__(self, msg: str, *args: P.args, **kwargs: P.kwargs) -> None:
+    def __init__(self, msg: Error, *args: P.args, **kwargs: P.kwargs) -> None:
         super().__init__(f"Domain invariant violated: {msg}", *args, **kwargs)
