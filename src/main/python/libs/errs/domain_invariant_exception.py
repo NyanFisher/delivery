f52806b973a007ee3e4f7eb73e104080b760ce3c
Максим Заикин <zaikinmaxv@gmail.com>
2026-02-27 18:12:01 +0500

    Fix circular import

--- src/main/python/libs/errs/domain_invariant_exception.py
+++
@@ -1,6 +0,0 @@
-from libs.errs.error import Error
-
-
-class DomainInvariantError[**P](Exception):
-    def __init__(self, msg: Error, *args: P.args, **kwargs: P.kwargs) -> None:
-        super().__init__(f"Domain invariant violated: {msg}", *args, **kwargs)
