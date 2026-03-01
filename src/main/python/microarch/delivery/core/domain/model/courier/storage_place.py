cfe1dc5c8eac24c15668c00a1e5692225bcf40af
Максим Заикин <zaikinmaxv@gmail.com>
2026-03-01 17:14:15 +0500

    Make fields private

--- src/main/python/microarch/delivery/core/domain/model/courier/storage_place.py
+++ src/main/python/microarch/delivery/core/domain/model/courier/storage_place.py
@@ -16,9 +16,21 @@ class StoragePlace(BaseEntity[uuid.UUID]):
         total_volume: int,
     ) -> None:
         super().__init__(uuid.uuid4())
-        self.name = name
-        self.total_volume = total_volume
-        self.order_id: uuid.UUID | None = None
+        self._name = name
+        self._total_volume = total_volume
+        self._order_id: uuid.UUID | None = None
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def total_volume(self) -> int:
+        return self._total_volume
+
+    @property
+    def order_id(self) -> uuid.UUID | None:
+        return self._order_id
 
     @classmethod
     def create(cls, name: str, total_volume: int) -> Result[typing.Self, Error]:
@@ -31,30 +43,30 @@ class StoragePlace(BaseEntity[uuid.UUID]):
 
     @property
     def is_occupied(self) -> bool:
-        return self.order_id is not None
+        return self._order_id is not None
 
     def can_store(self, volume: int) -> bool:
-        return self.order_id is None and volume <= self.total_volume
+        return self._order_id is None and volume <= self._total_volume
 
     def store(self, order_id: uuid.UUID, volume: int) -> UnitResult[Error]:
-        if self.order_id is not None:
+        if self._order_id is not None:
             err = Error("already.contains.another.order", "The storage location already contains another order")
             return UnitResult.failure(err)
         if err := Guard.against_null_or_empty_uuid(order_id, "order_id"):  # type: ignore[assignment]
             return UnitResult.failure(err)
-        if err := Guard.against_out_of_range(volume, self.MIN_TOTAL_VOLUME, self.total_volume, "volume"):  # type: ignore[assignment]
+        if err := Guard.against_out_of_range(volume, self.MIN_TOTAL_VOLUME, self._total_volume, "volume"):  # type: ignore[assignment]
             return UnitResult.failure(err)
 
-        self.order_id = order_id
+        self._order_id = order_id
         return UnitResult.success()
 
     def clear(self, order_id: uuid.UUID) -> UnitResult[Error]:
         if err := Guard.against_null_or_empty_uuid(order_id, "order_id"):
             return UnitResult.failure(err)
 
-        if self.order_id is None or self.order_id != order_id:
+        if self._order_id is None or self._order_id != order_id:
             err = Error("order.id.does.not.match", "The order ID does not match the one stored in the storage place")
             return UnitResult.failure(err)
 
-        self.order_id = None
+        self._order_id = None
         return UnitResult.success()
--- src/test/python/microarch/delivery/core/domain/model/courier/test_storage_place.py
+++ src/test/python/microarch/delivery/core/domain/model/courier/test_storage_place.py
@@ -6,12 +6,26 @@ from microarch.delivery.core.domain.model.courier.storage_place import StoragePl
 from pytest_mock import MockerFixture
 
 STORAGE_PLACE_UUID = uuid.uuid4()
+STORAGE_PLACE_NAME = "backpack"
+STORAGE_PLACE_VOLUME = 20
+ORDER_UUID = uuid.uuid4()
 
 
 class TestStoragePlace:
+    def assert_storage_place(
+        self,
+        storage: StoragePlace,
+        expected_name: str = STORAGE_PLACE_NAME,
+        expected_volume: int = STORAGE_PLACE_VOLUME,
+        expected_order_id: uuid.UUID | None = None,
+    ) -> None:
+        assert storage.name == expected_name
+        assert storage.total_volume == expected_volume
+        assert storage.order_id == expected_order_id
+
     @pytest.fixture
     def sut(self) -> StoragePlace:
-        return StoragePlace.create(name="backpack", total_volume=20).value
+        return StoragePlace.create(name=STORAGE_PLACE_NAME, total_volume=STORAGE_PLACE_VOLUME).value
 
     def test_equal(self, mocker: MockerFixture) -> None:
         mock_uuid4 = mocker.patch("microarch.delivery.core.domain.model.courier.storage_place.uuid.uuid4")
@@ -37,28 +51,26 @@ class TestStoragePlace:
         result = StoragePlace.create(name=name, total_volume=total_volume)
 
         assert result.is_failure
-        assert expected in result.error.serialize()
+        assert result.error.code == expected
 
     def test_success_create_storage_place(self) -> None:
         result = StoragePlace.create(name="backpack", total_volume=20)
 
         assert result.is_success
-        assert result.value.id_ is not None
-        assert result.value.name == "backpack"
-        assert result.value.total_volume == 20
+        self.assert_storage_place(result.value)
 
     @pytest.mark.parametrize(("volume", "expected"), [(10, True), (25, False)])
     def test_can_store(self, volume: int, expected: bool, sut: StoragePlace) -> None:
         assert sut.can_store(volume) is expected
 
     def test_store(self, sut: StoragePlace) -> None:
-        result = sut.store(STORAGE_PLACE_UUID, 10)
+        result = sut.store(ORDER_UUID, 10)
 
         assert result.is_success
-        assert sut.order_id is STORAGE_PLACE_UUID
+        self.assert_storage_place(sut, expected_order_id=ORDER_UUID)
 
     def test_failure_store_if_volume_exceeds_total_volume(self, sut: StoragePlace) -> None:
-        result = sut.store(STORAGE_PLACE_UUID, 25)
+        result = sut.store(ORDER_UUID, 25)
 
         assert result.is_failure
         assert result.error.code == "value.is.out.of.range"
@@ -66,20 +78,20 @@ class TestStoragePlace:
 
     @pytest.mark.parametrize("volume", [-1, 0])
     def test_failure_store_if_unacceptable_volume(self, volume: int, sut: StoragePlace) -> None:
-        result = sut.store(STORAGE_PLACE_UUID, volume)
+        result = sut.store(ORDER_UUID, volume)
 
         assert result.is_failure
         assert result.error.code == "value.is.out.of.range"
         assert sut.order_id is None
 
     def test_failure_store_if_already_contains_another_order(self, sut: StoragePlace) -> None:
-        sut.store(STORAGE_PLACE_UUID, 19)
+        sut.store(ORDER_UUID, 19)
 
         result = sut.store(uuid.uuid4(), 26)
 
         assert result.is_failure
         assert result.error.code == "already.contains.another.order"
-        assert sut.order_id == STORAGE_PLACE_UUID
+        assert sut.order_id == ORDER_UUID
 
     def test_failure_store_if_order_id_is_empty_uuid(self, sut: StoragePlace) -> None:
         result = sut.store(Guard.EMPTY_UUID, 20)
@@ -89,15 +101,15 @@ class TestStoragePlace:
         assert sut.order_id is None
 
     def test_clear(self, sut: StoragePlace) -> None:
-        sut.store(STORAGE_PLACE_UUID, 10)
+        sut.store(ORDER_UUID, 10)
 
-        result = sut.clear(STORAGE_PLACE_UUID)
+        result = sut.clear(ORDER_UUID)
 
         assert result.is_success
         assert sut.order_id is None
 
     def test_failure_clear_if_there_is_no_order_id_in_the_storage_place(self, sut: StoragePlace) -> None:
-        result = sut.clear(STORAGE_PLACE_UUID)
+        result = sut.clear(ORDER_UUID)
 
         assert result.is_failure
         assert result.error.code == "order.id.does.not.match"
@@ -111,18 +123,18 @@ class TestStoragePlace:
         assert sut.order_id is None
 
     def test_failure_clear_if_order_id_doesnt_match(self, sut: StoragePlace) -> None:
-        sut.store(STORAGE_PLACE_UUID, 10)
+        sut.store(ORDER_UUID, 10)
 
         result = sut.clear(uuid.uuid4())
 
         assert result.is_failure
         assert "order.id.does.not.match" in result.error.serialize()
-        assert sut.order_id == STORAGE_PLACE_UUID
+        assert sut.order_id == ORDER_UUID
 
     def test_is_not_occupied(self, sut: StoragePlace) -> None:
         assert sut.is_occupied is False
 
     def test_is_occupied(self, sut: StoragePlace) -> None:
-        sut.store(STORAGE_PLACE_UUID, 10)
+        sut.store(ORDER_UUID, 10)
 
         assert sut.is_occupied is True
