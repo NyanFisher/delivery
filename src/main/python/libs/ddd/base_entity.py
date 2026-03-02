1d99e583181debf060c700c4ef79faf800092cbf
Максим Заикин <zaikinmaxv@gmail.com>
2026-03-02 11:04:20 +0500

    Revert

--- src/main/python/libs/ddd/base_entity.py
+++ src/main/python/libs/ddd/base_entity.py
@@ -9,7 +9,7 @@ class BaseEntity[TId: Any]:
     def id_(self) -> TId | None:
         return self._id
 
-    def is_transient(self) -> bool:
+    def _is_transient(self) -> bool:
         return self._id is None or self._id == self._default_value()
 
     def _default_value(self) -> TId | None:
@@ -26,7 +26,7 @@ class BaseEntity[TId: Any]:
         if type(self) is not type(other):
             return False
 
-        if self.is_transient() or other.is_transient():
+        if self._is_transient() or other._is_transient():
             return False
 
         return self._id == other._id
@@ -41,7 +41,7 @@ class BaseEntity[TId: Any]:
         if self is other:
             return False
 
-        if self.is_transient() or other.is_transient():
+        if self._is_transient() or other._is_transient():
             return False
 
         return self._id < other._id  # type: ignore[operator]
